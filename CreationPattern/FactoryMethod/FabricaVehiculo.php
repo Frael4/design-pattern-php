a3fe9458cae6d725f294aa4e1ae4e2fbcacf0c33
Frael98 <frael1984@gmail.com>
2025-09-27 15:23:45 -0500

    fix: Comentarios...

--- CreationPattern/FactoryMethod/FabricaVehiculo.php
+++ CreationPattern/FactoryMethod/FabricaVehiculo.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace FactoryMethod;
+
+/**
+ * Clase Fabricadora Padre
+ */
 abstract class FabricaVehiculo{
     
     // Factory Metodo
     public abstract function crearVehiculo() : Vehiculo;
 
+    // Método que usa el Factory Metodo
     public function iniciarProduccion(){
         $vehiculo = $this->crearVehiculo();
         $vehiculo->conducir();
